0c4799a816874256669863481ca1ac400986df3a
LonneQuent <lonnequentin@gmail.com>
2023-12-19 14:13:21 +0100

    V1_log_in_py

---
+++ log_in.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+company = input("Enter your company: ")
+job = input("Enter your job: ")
+
+print("\nInformation entered:")
+print("Name:", name)
+print("Company:", company)
+print("Job:", job)
